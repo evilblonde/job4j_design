dc97ea9534874ee0867c0a5860514418bd6473fc
n.kolesnik <nikita.koles@gmail.com>
2020-03-14 16:32:42 +0300

    several threads

---
+++ chapter_007/src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+
+    public static void main(String[] args) {
+        Thread first = new Thread(ConcurrentOutput::printThreadName);
+        Thread second = new Thread(ConcurrentOutput::printThreadName);
+        Thread third = new Thread(ConcurrentOutput::printThreadName);
+        Thread fourth = new Thread(ConcurrentOutput::printThreadName);
+        Thread fifth = new Thread(ConcurrentOutput::printThreadName);
+        Thread sixth = new Thread(ConcurrentOutput::printThreadName);
+        first.start();
+        second.start();
+        third.start();
+        fourth.start();
+        fifth.start();
+        sixth.start();
+        printThreadName();
+    }
+
+    private static void printThreadName() {
+        System.out.println(Thread.currentThread().getName());
+    }
+
+}
