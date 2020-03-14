2b5ce15c449cde67ff0c1254ff3779650d0ca1cc
n.kolesnik <nikita.koles@gmail.com>
2020-03-14 20:07:30 +0300

    count

---
+++ chapter_007/src/test/java/ru/job4j/sync/CountTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.sync;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountTest {
+
+    @Test
+    public void whenExecute2ThreadsThenCountIs2() throws InterruptedException {
+        final Count count = new Count();
+
+        Thread thr1 = new ThreadCount(count);
+        Thread thr2 = new ThreadCount(count);
+
+        thr1.start();
+        thr2.start();
+
+        thr1.join();
+        thr2.join();
+
+        assertEquals(2, count.get());
+    }
+
+    private class ThreadCount extends Thread {
+        private final Count count;
+
+        private ThreadCount(final Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+}
